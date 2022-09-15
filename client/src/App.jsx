4c49bd25f1cdf4887237e42d3ce361beb648b169
Chris Mittell <mittell@hotmail.co.uk>
2022-09-15 11:11:50 +0900

    feat: added Graph component and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import Graph from './components/Graph';
+
 function App() {
-	return <div></div>;
+	return (
+		<div className='app'>
+			<div className='container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800'>
+				<h1 className='text-4xl py-8 mb-10 bg-slate-800 text-white rounded'>
+					Expense Tracker
+				</h1>
+				<div className='grid md:grid-cols-2 gap-4'>
+					<Graph />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
